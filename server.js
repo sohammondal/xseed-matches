e17c6a41e158b4fcc7197d58212507c284034f7f
sohammondal <soham.pf.mondal@gmail.com>
2019-11-27 21:24:19 +0530

    add color to console log

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ mongodb.connect(process.env.MONGODB_URI);
 
 
 // START THE SERVER
-const listener = app.listen(3000, () => console.log('LISTENING ON PORT', listener.address().port));
+const listener = app.listen(process.env.PORT || 3000, () => console.log('\x1b[32m%s\x1b[0m', 'LISTENING ON PORT', listener.address().port));
