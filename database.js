c2491ae64ae0e90778eaa4a6c2a0a24be3c653b9
sohammondal <soham.pf.mondal@gmail.com>
2019-11-27 21:23:53 +0530

    add color to console log & handle blank mongo uri

--- database.js
+++ database.js
@@ -1,17 +1,24 @@
 const mongoose = require('mongoose');
 module.exports = {
     mongoose,
-    connect: (uri) => mongoose.connect(uri, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true
-    }, (err) => {
-        if (err) {
-            console.error(err.name, ':', err.message);
+    connect: (uri) => {
+        if (uri) {
+            mongoose.connect(uri, {
+                useNewUrlParser: true,
+                useCreateIndex: true,
+                useUnifiedTopology: true
+            }, (err) => {
+                if (err) {
+                    console.error(err.name, ':', err.message);
+                } else {
+                    console.log('\x1b[32m%s\x1b[0m', 'MONGODB CONNECTED')
+                }
+            })
         } else {
-            console.log('MONGODB CONNECTED')
+            console.error('\x1b[31m%s\x1b[0m', 'ERROR: MONGODB CONNECTION FAILED.');
+            console.warn('\x1b[33m%s\x1b[0m', 'You need pass a valid MONGODB URI in a .env file/server.js')
         }
-    }),
+    },
     disconnect: (done) => {
         mongoose.disconnect(done);
     }
